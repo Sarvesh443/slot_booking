59a74f941a9913b5bae16258aab127e61a7b406c
Sarvesh443 <sarveshkumar.2496@gmail.com>
2023-08-09 10:17:02 +0530

    Added end_date to session

--- app.js
+++ app.js
@@ -124,6 +124,23 @@ try {
       // save user token
       user.token = token;
 
+      const slots = await DeanSession.find();
+      slots.forEach(async slotPiece => {
+        const { _id, slot, day } = slotPiece;
+        const updateSlot = new DeanSession({
+            _id,
+            slot,
+            day,
+            status: 'available',
+            booked_by: 'Null'
+        });
+        const option = { new: false };
+
+        if(!slot.end_time || slot.end_time > Date.now){
+            await DeanSession.findOneAndUpdate(_id, updateSlot, option);
+        }
+      });
+
       // user
       res.status(200).json(user);
     }
@@ -151,32 +168,28 @@ app.get("/sessions", auth, async (req, res) => {
     const dean = user.substring(user.indexOf('@') + 1, user.indexOf('.'));
     if(dean == 'dean'){
         const deanSession = await DeanSession.find({});
-        res.status(400).send(deanSession);
+        res.status(200).send(deanSession);
+    } else {
+        const allSessions = await DeanSession.find({}, {booked_by: 0});
+        res.status(200).send(allSessions);
     }
-    const allSessions = await DeanSession.find({}, {booked_by: 0});
-    res.status(200).send(allSessions);
 });
 
 app.post("/book/:slot", auth, async (req, res) => {
     const slot = req.params.slot;
-    /**
-     * if(slot is available)
-     *  book the slot
-     * else {
-     *  see if other slots are available
-     *  else  throw error - info: book the available slot
-     * 
-     * }
-     */
+
     const session = await DeanSession.findOne({slot: slot, status: 'available'});
     if(session){
         const { _id, slot, day } = session;
+        const end_time = new Date(this.start_time.getTime() + 60 * 60 * 1000);
         const updateSlot = new DeanSession({
             _id,
             slot,
             day,
             status: 'booked',
-            booked_by: req.user.email
+            booked_by: req.user.email,
+            start_time: Date.now,
+            end_time: end_time
         });
         const option = { new: true };
 
@@ -185,11 +198,10 @@ app.post("/book/:slot", auth, async (req, res) => {
         res.status(200).send(bookedSession);
     } else {
         const availableSession = await DeanSession.findOne({status: 'available'});
-        if(availableSession) res.status(400).send(availableSession);
-        else{
-            console.log("No Session Found!");
+        if(availableSession) 
+            res.status(400).send(availableSession);
+        else
             res.status(400).send("No Session Available with the Dean!");
-        }
     }
     // const sessions = await DeanSession.find({slot: slot});
     // res.status(200).send(sessions);
--- model/deanSession.js
+++ model/deanSession.js
@@ -23,7 +23,16 @@ const DeanSessionSchema = new mongoose.Schema({
   start_time: {
     type: Date,
     default: Date.now
+  },
+  end_time: {
+    type: Date
   }
+});
+
+// Middleware to set the end_date to 1 hour after bookingDate
+DeanSessionSchema.pre('save', function (next) {
+  this.end_time = new Date(this.start_time.getTime() + 60 * 60 * 1000); // Adding 1 hour in milliseconds
+  next();
 });
 
 const Session = mongoose.model("session", DeanSessionSchema);
